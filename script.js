7e755313a74b9193fb4649354f4593bfa2d21482
Tommy Nguyen <Tommy.nguyenoc@gmail.com>
2023-02-17 09:09:12 +0100

    added function funny text

--- script.js
+++ script.js
@@ -17,4 +17,24 @@ function darkmode() {
     }
 //  }
 
- /* -------------------- */ 
+ /* --------If sats for funny text --------- */ 
+
+let temp = document.querySelector(".temperature")
+let funnyText = document.querySelector("#information-text")
+
+let tempNum = parseInt(temp.innerHTML)
+
+
+if (tempNum>=20){
+    funnyText.innerText = "Perfekt väder för att ta en öl i solen, glöm inte solglasögonen!"
+} else if (tempNum<0) {
+    funnyText.innerText = "Kallt ute! Räkna med att frysa om inte du har varma kläder!"
+} else if (tempNum<20 || tempNum>0){
+    funnyText.innerText = "Nu är vädret lagom!"
+} else {
+    funnyText.innerText = "Vädret är oförutsägbart!"
+}
+
+console.log(tempNum)
+
+
