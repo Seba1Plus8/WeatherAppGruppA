bd6f52ecc6bfe55e2303936459b06daa02f29a20
SeCo <s.con3ras1993@gmail.com>
2023-02-28 14:56:07 +0100

    la in detailed

--- Assets/Scripts/Favourites.js
+++ Assets/Scripts/Favourites.js
@@ -83,11 +83,11 @@ function showFavouriteCity(list) {
             hours = data.hourly.time;
             temps = data.hourly.temperature_2m;
             windspeed = data.hourly.windspeed_10m;
-            precipitation = data.hourly.precipitation;
+            precipitations = data.hourly.precipitation;
             index = hours.indexOf(`${year}-0${month}-${day}T${hour}:00`)
             temp = temps.at(index);
             wind = windspeed.at(index)
-            precipitation = precipitation.at(index)
+            precipitation = precipitations.at(index)
             Math.round(wind);
             Math.round(temp);
             Math.round(precipitation);
@@ -108,9 +108,29 @@ function showFavouriteCity(list) {
             const h4 = document.createElement("h2");
             h4.innerHTML = `${precipitation}`
             ul.append(h4)
-
-        
             
+
+            feels = data.hourly.apparent_temperature;
+            Math.round(temp);
+
+            for (let i = 0;i < 25; i++) {
+                const element = hours[i];
+                const li = document.createElement("li");
+                li.innerHTML = `
+                <div class="upper-Box">
+                <h2>${i.toLocaleString("se", {minimumIntegerDigits:2})}:00</h2> 
+                <h3>${Math.round(temps[i])}C</h3>
+                <h6>${Math.round(windspeed[i])}m/s</h6>
+                </div>
+                <div class="lower-Box">
+                <h6>${precipitations[i]}mm</h6>
+               <img src="/550c1520618de7f49b3ca4f8d92fcc4d.png" class="icon" alt="vädret">
+                <h6>${Math.round(feels[i])}C</h6>
+                </div>
+                ` 
+                ul.append(li);
+            }
+
 
         }
 
